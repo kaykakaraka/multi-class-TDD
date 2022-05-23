9a23b87a011a73367f6416d398dcf57988983aae
Kay <kard@hotmail.co.uk>
2022-05-23 11:26:13 +0100

    added find best entry for reading time

--- lib/diary.rb
+++ lib/diary.rb
@@ -21,19 +21,14 @@ class Diary
     counter
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
+  def reading_time(wpm) 
+    count_words / wpm                 
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
+    words_to_read = wpm * minutes
+    entries = @DiaryEntries
+    entries.select! { |entry| entry.count_words <= words_to_read }
+    entries.max_by { |entry| entry.count_words }
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -25,6 +25,20 @@ RSpec.describe "integration" do
       diary.add(entry_1)
       expect(diary.count_words).to eq 8
     end
+
+    it "gives the number of minutes needed to read the entry" do
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "I went for a lovely swim at the park")
+      diary.add(entry_1)
+      expect(diary.reading_time(1)).to eq 9
+    end
+
+    it "gives the number of minutes needed to read a longer entry" do
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "contents " * 900)
+      diary.add(entry_1)
+      expect(diary.reading_time(50)).to eq 18
+    end
   end
 
   context "When two diary entries are added to diary" do
@@ -45,8 +59,48 @@ RSpec.describe "integration" do
       diary.add(entry_2)
       expect(diary.count_words).to eq 8
     end
+
+    it "gives the number of minutes needed to read both entries" do
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "contents " * 900)
+      entry_2 = DiaryEntry.new("Friday", "contents " * 800)
+      diary.add(entry_1)
+      diary.add(entry_2)
+      expect(diary.reading_time(50)).to eq 34
+    end
   end
 
-  
- 
+  context "both diary entries could be read by the user in time" do
+    it "selects the diary entry that is closest to the length they can read" do
+      diary = Diary.new
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "contents " * 900)
+      entry_2 = DiaryEntry.new("Friday", "contents " * 800)
+      diary.add(entry_1)
+      diary.add(entry_2)
+      expect(diary.find_best_entry_for_reading_time(50, 20)).to eq entry_1
+    end
+
+    it "selects a different diary entry that is closest to the length they can read" do
+      diary = Diary.new
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "contents " * 50)
+      entry_2 = DiaryEntry.new("Friday", "contents " * 800)
+      diary.add(entry_1)
+      diary.add(entry_2)
+      expect(diary.find_best_entry_for_reading_time(50, 20)).to eq entry_2
+    end
+  end
+
+  context "one diary entry is too long for the user to read" do
+    it "returns the entry the user has time to read" do
+      diary = Diary.new
+      diary = Diary.new
+      entry_1 = DiaryEntry.new("Thursday", "I walked the dog")
+      entry_2 = DiaryEntry.new("Friday", "I was walking the dog very happily today")
+      diary.add(entry_1)
+      diary.add(entry_2)
+      expect(diary.find_best_entry_for_reading_time(4, 1)).to eq entry_1
+    end
+  end
 end
